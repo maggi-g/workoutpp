d1fcd354bfd70b18de9d5568b5e37d1e7b09e750
Meghana Gandra <gandra@hpe.com>
2019-05-17 17:21:08 +0530

    Added Database,Class libraries,angular project

---
+++ WorkOutSinglePageApp/DataScripts.sql
@@ -0,0 +1,36 @@
+﻿create database WorkoutSPA
+
+use WorkoutSPA
+
+
+
+
+create table workoutcollection
+(
+workouttitle  varchar(128),
+workoutnote   varchar(256), 
+caloriesburnpermin   float, 
+categoryid    int    ,
+workoutid     int    Primary key identity,
+constraint fk_workoutcatagory_categoryid foreign key(categoryid) references workoutcategory(categoryid)
+)
+
+
+
+create table workoutactive(
+id int identity primary key ,
+workoutid     int    ,
+starttime     time,  
+startdate     date,  
+enddate       date,  
+endtime       time,  
+comment       varchar(64)   , 
+currentstatus bit,
+constraint fk_workoutcollection_workoutid foreign key(workoutid)
+references workoutcollection(workoutid) 
+       )
+
+
+create table workoutcategory(
+categoryid    int    Primary Key identity,
+categoryname varchar(64))
---
+++ WorkOutSinglePageApp/WorkOutBusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkOutBusinessLogic
+{
+    public class Class1
+    {
+    }
+}
